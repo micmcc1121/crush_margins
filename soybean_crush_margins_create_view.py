f5d3a82c13e83a33480afa7dcb0fb34d54672193
Michael McConnell <michael@devlaptop.localdomain>
2025-10-08 22:20:29 -0400

    added oil and meal value share columns to the view

--- soybean_crush_margins_create_view.py
+++ soybean_crush_margins_create_view.py
@@ -1,3 +1,23 @@
+'''
+Script to create the location-level soybean cash crush margin 
+view in the agmktdb Postgresql Database
+
+created by: Michael McConnell
+created on: October 8, 2025
+
+To do:
+    1)
+    2)
+'''
+
+# Libraries
+from sqlalchemy import create_engine
+
+# Create connection
+
+# SQL statement
+
+stmt = '''
 WITH soybean_costs AS (
     SELECT 
         date_trunc('week', report_begin_date) AS week_of, 
@@ -28,8 +48,6 @@ soybean_revenues AS (
         m2.avg_price AS soy_meal_price, 
         m1.trans_mode AS soy_oil_transmode,
         m2.trans_mode as soy_meal_transmode, 
-        ROUND(CAST(m1.avg_price * 11.6/100 AS numeric), 2) AS soy_oil_revenues,
-        ROUND(CAST(m2.avg_price * 47.1/2000 AS numeric), 2) AS soy_meal_revenues,
         ROUND(CAST((m1.avg_price * 11.6/100) + (m2.avg_price * 47.1/2000) AS numeric), 2) AS crush_revenues
     FROM mars_proc_prices AS m1
     INNER JOIN mars_proc_prices AS m2
@@ -39,21 +57,13 @@ soybean_revenues AS (
         AND m2.commodity IN ('Soybean Meal')
     ORDER BY m1."trade Loc", week_of desc
 )
-SELECT 
-    r.week_of AS week_of, 
-    r."trade Loc" AS trade_loc, 
-    c.trans_mode AS soy_transmode, 
-    r.soy_oil_transmode AS soy_oil_transmode, 
-    r.soy_meal_transmode AS soy_meal_transmode, 
-    c.crush_costs AS soy_costs,
-    r.soy_oil_revenues AS soy_oil_revenues,
-    r.soy_meal_revenues AS soy_meal_revenues,
-    r.crush_revenues AS crush_revenues, 
-    c.crush_costs AS crush_costs, 
-    r.crush_revenues - c.crush_costs AS crush_margin,
-    ROUND(CAST(r.soy_oil_revenues/r.crush_revenues * 100 AS numeric), 2) AS crush_oil_share,
-    ROUND(CAST(r.soy_meal_revenues/r.crush_revenues * 100 AS numeric), 2) AS crush_meal_share
-FROM soybean_revenues as r
-LEFT JOIN soybean_costs as c
-ON r.week_of = c.week_of AND r."trade Loc" = c."trade Loc"
-;
+CREATE VIEW soybean_cash_crush_margins AS (
+    SELECT r.week_of AS week_of, r."trade Loc" AS trade_loc, c.trans_mode AS soy_transmode, r.soy_oil_transmode AS soy_oil_transmode, r.soy_meal_transmode AS soy_meal_transmode, r.crush_revenues AS crush_revenues, c.crush_costs AS crush_costs, r.crush_revenues - c.crush_costs AS crush_margin
+    FROM soybean_revenues as r
+    LEFT JOIN soybean_costs as c
+    ON r.week_of = c.week_of AND r."trade Loc" = c."trade Loc"
+    ;
+)
+'''
+
+# Execute statement
